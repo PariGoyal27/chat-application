433e09dabae7d31ac85a1dcd74883299e6a9b672
Pari-Goyl <goyalpari678@gmail.com>
2026-05-29 17:32:32 +0530

    Added background and added createBrowerRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import HomePage from "./components/HomePage";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      Let's build chat
-      <button className="btn btn-secondary">Secondary</button>
+      <RouterProvider router={router} />
     </div>
   );
 }
