d4ef7011d6e95e706d51867becc6803122a4e86d
Pari-Goyl <goyalpari678@gmail.com>
2026-05-29 16:57:50 +0530

    Setup React frontend with Tailwind CSS and DaisyUIv4

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Let's build chat
+      <button className="btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
+
+export default App;
