e8944d0cd76cbe48bd7e8f707d3e874166244663
Pari-Goyl <goyalpari678@gmail.com>
2026-06-08 10:51:49 +0530

    in messageContainer getting selected user's full name and profile photo

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import SendInput from "./SendInput";
 import Messages from "./Messages";
+import { useSelector } from "react-redux";
 
 const MessageContainer = () => {
+  const { selectedUser } = useSelector((store) => store.user);
   return (
     <div className="md:min-w-[550px] flex flex-col">
       <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2">
         <div className="avatar online">
           <div className="w-11 rounded-full">
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjtHioeP3798yMa6QIJsA3piLZlDdOMuA17Q&s"
+              src={selectedUser?.profilePhoto}
               alt="user-profile"
             />
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex justify-between gap-2">
-            <p>Pari Goyal</p>
+            <p>{selectedUser?.fullName}</p>
           </div>
         </div>
       </div>
