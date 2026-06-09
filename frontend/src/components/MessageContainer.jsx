d6a2e8c32e7b70cde8f13247aec8400908c29928
Pari-Goyl <goyalpari678@gmail.com>
2026-06-09 17:05:06 +0530

    Added redux-persistance

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -5,8 +5,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUser } from "../redux/userSlice";
 
 const MessageContainer = () => {
-  const { selectedUser,authUser } = useSelector((store) => store.user);
+  const { selectedUser, authUser, onlineUsers } = useSelector(
+    (store) => store.user,
+  );
   const dispatch = useDispatch();
+
+  const isOnline = onlineUsers.includes(selectedUser?._id)
+
   useEffect(() => {
     return () => dispatch(setSelectedUser(null));
   }, []);
@@ -15,7 +20,7 @@ const MessageContainer = () => {
       {selectedUser !== null ? (
         <div className="md:min-w-[550px] flex flex-col">
           <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2">
-            <div className="avatar online">
+            <div className={`avatar ${isOnline ? "online" : ""}`}>
               <div className="w-11 rounded-full">
                 <img src={selectedUser?.profilePhoto} alt="user-profile" />
               </div>
@@ -31,7 +36,9 @@ const MessageContainer = () => {
         </div>
       ) : (
         <div className="md:min-w-[550px] flex flex-col justify-center items-center">
-          <h1 className="text-3xl text-white font-bold">Hi, {authUser?.fullName || "User"}</h1>
+          <h1 className="text-3xl text-white font-bold">
+            Hi, {authUser?.fullName || "User"}
+          </h1>
           <h1 className="text-2xl text-white">Let's start a conversation</h1>
         </div>
       )}
